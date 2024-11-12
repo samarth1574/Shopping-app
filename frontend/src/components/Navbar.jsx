9948c5aaad3ea843b2462f6e91eedc2b91cb9751
samarth1574 <shekharsamarth1574@gmail.com>
2024-11-12 23:43:17 +0530

    f

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,9 +45,9 @@ const Navbar = () => {
       <div className='flex items-center gap-6'>
             <img onClick={()=> { setShowSearch(true); navigate('/collection') }} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
             
-            <div className='group relative'>
+           {/* <div className='group relative'>
                 <img onClick={()=> token ? null : navigate('/login') } className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
-                {/* Dropdown Menu */}
+                {/* Dropdown Menu }
                 {token && 
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5  bg-slate-100 text-gray-500 rounded'>
@@ -56,7 +56,7 @@ const Navbar = () => {
                         <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
                     </div>
                 </div>}
-            </div> 
+            </div> */}
             <Link to='/cart' className='relative'>
                 <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
                 <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
