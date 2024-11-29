0139198cad7f1c3ed154596714b3bd4b1d0a1006
samarth1574 <shekharsamarth1574@gmail.com>
2024-11-29 14:05:41 +0530

    minor fixes

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
           <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@forever.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          
         </div>
       </div>
 
